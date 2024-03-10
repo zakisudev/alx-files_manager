370adfc28e32d55877d3ea544d25c3eb679c2395
zakisu250 <zakisu250@gmail.com>
2024-03-10 11:46:52 +0300

    Fix formatting and add missing commas in AppController, AuthController, FilesController, and UsersController

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -4,53 +4,53 @@ import {
   generateSessionToken,
   getBasicAuthToken,
   getCurrentUser,
-  getSessionToken
+  getSessionToken,
 } from '../utils/auth';
 
 /**
  * AuthController class to handle authentication
  */
 class AuthController {
-  static async getConnect (request, response) {
+  static async getConnect(request, response) {
     const { email, password } = getBasicAuthToken(request);
     if (!email || !password) {
       return response.status(401).json({
-        error: 'Unauthorized'
+        error: 'Unauthorized',
       });
     }
 
     const user = await authenticateUser(email, password);
     if (!user) {
       return response.status(401).json({
-        error: 'Unauthorized'
+        error: 'Unauthorized',
       });
     }
     const token = await generateSessionToken(user._id);
     return response.status(200).json(token);
   }
 
-  static async getDisconnect (request, response) {
+  static async getDisconnect(request, response) {
     const token = getSessionToken(request);
     if (!token) {
       return response.status(401).json({
-        error: 'Unauthorized'
+        error: 'Unauthorized',
       });
     }
 
     const result = await deleteSessionToken(token);
     if (!result) {
       return response.status(401).json({
-        error: 'Unauthorized'
+        error: 'Unauthorized',
       });
     }
     return response.sendStatus(204);
   }
 
-  static async getMe (request, response) {
+  static async getMe(request, response) {
     const currentUser = await getCurrentUser(request);
     if (!currentUser) {
       return response.status(401).json({
-        error: 'Unauthorized'
+        error: 'Unauthorized',
       });
     }
     return response.status(200).json(currentUser);
