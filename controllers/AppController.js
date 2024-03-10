6bdf9d5134c9b55c3ba5f2e7756808981e2939d2
zakisu250 <zakisu250@gmail.com>
2024-03-10 11:18:42 +0300

    Add AppController with getStatus and getStats methods

---
+++ controllers/AppController.js
@@ -0,0 +1,31 @@
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+/**
+ * AppController class
+ */
+class AppController {
+  /**
+   * getStatus - check the status of the API
+   * @param {Object} request - request object
+   * @param {Object} response - response object
+   * @return {void}
+   */
+  static getStatus (request, response) {
+    response.statusCode = 200;
+    response.send({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive()
+    });
+  }
+
+  static async getStats (request, response) {
+    response.statusCode = 200;
+    response.send({
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles()
+    });
+  }
+}
+
+export default AppController;
