2f4dafc47777381b26d24fb0427128693f2819c7
zakisu250 <zakisu250@gmail.com>
2024-03-10 11:19:02 +0300

    Add UsersController class with postNew method

---
+++ controllers/UsersController.js
@@ -0,0 +1,29 @@
+import dbClient from '../utils/db';
+import { userQueue } from '../worker';
+
+/**
+ * UsersController class
+ */
+class UsersController {
+  static async postNew (request, response) {
+    const { email, password } = request.body;
+
+    if (!email) {
+      return response.status(400).json({ error: 'Missing email' });
+    }
+    if (!password) {
+      return response.status(400).json({ error: 'Missing password' });
+    }
+
+    const existingUser = await dbClient.findUserByEmail(email);
+    if (existingUser) {
+      return response.status(400).json({ error: 'Already exist' });
+    }
+
+    const newUser = await dbClient.addUser(email, password);
+    userQueue.add({ userId: newUser.id });
+    return response.status(201).json(newUser);
+  }
+}
+
+export default UsersController;
