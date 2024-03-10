583d054b44faa527fd93b05acc932a5810522ab9
zakisu250 <zakisu250@gmail.com>
2024-03-10 11:41:17 +0300

    Refactor AppController methods

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -11,19 +11,19 @@ class AppController {
    * @param {Object} response - response object
    * @return {void}
    */
-  static getStatus (request, response) {
+  static getStatus(request, response) {
     response.statusCode = 200;
     response.send({
       redis: redisClient.isAlive(),
-      db: dbClient.isAlive()
+      db: dbClient.isAlive(),
     });
   }
 
-  static async getStats (request, response) {
+  static async getStats(request, response) {
     response.statusCode = 200;
     response.send({
       users: await dbClient.nbUsers(),
-      files: await dbClient.nbFiles()
+      files: await dbClient.nbFiles(),
     });
   }
 }
